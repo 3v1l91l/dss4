d25b9e09f98e7fb5dbfdac71feca92d7a081f07c
Misha Zhukov <3v1l91l@gmail.com>
2018-02-13 08:11:25 +0200

    m

--- photo_features.py
+++ photo_features.py
@@ -8,7 +8,7 @@ model = InceptionV3(include_top=False, weights='imagenet', input_shape=(299,299,
 users_df, _ = load_data()
 
 filepaths = list(map(lambda x: os.path.join('processed_photos', str(x) + '.jpg'), users_df.index.values))
-imgs = list(map(Image.open, filepaths))
 for i in range(len(users_df)):
-    users_df.iloc[i, 'photo_feature'] = model.predict(imgs[i])
+    img = Image.open(filepaths[i])
+    users_df.iloc[i, 'photo_feature'] = model.predict(np.array(img))
 print('done')
