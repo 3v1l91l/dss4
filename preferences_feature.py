513f170c29cd2fe1e916d2eacdaeb253d0b692f0
Misha Zhukov <3v1l91l@gmail.com>
2018-02-13 16:52:23 +0200

    m

--- preferences_feature.py
+++ preferences_feature.py
@@ -1,11 +1,11 @@
 from lib import load_data
 import pandas as pd
+import numpy as np
 
 _, finder_decisions = load_data()
 
 users_df = pd.read_pickle('users_df')
-finder_decisions.merge(users_df['feature'].to_frame(), how='left', left_on='Receiver_id', left_index=True)
-gr = finder_decisions.groupby('Sender_id').agg({'feature': lambda x: x.sum(axis=0) / len(x)})
-
+finder_decisions = finder_decisions.merge(users_df['feature'].to_frame(), how='left', left_on='Receiver_id', right_index=True)
+gr = finder_decisions.groupby('Sender_id')['feature'].apply(np.mean)
 users_df['preferences'] = None
 # for id in users_df.index.values:
