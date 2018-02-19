716b2b100513f83356bc9c1b50cca214405a1520
Misha Zhukov <3v1l91l@gmail.com>
2018-02-19 09:02:46 +0200

    m

--- keras_matrix_factorization.py
+++ keras_matrix_factorization.py
@@ -36,15 +36,24 @@ users = pd.read_pickle('users_df')
 # print((predictions.values == finder_decisions_valid['Decision'].values).sum()/len(predictions))
 
 finder_decisions['Decision'] = finder_decisions['Decision'] == 'like'
-zz = users.index.unique()[:20000]
-users.drop(users.index[~users.index.isin(zz)], inplace=True)
-finder_decisions.drop(finder_decisions.index[~finder_decisions['Sender_id'].isin(zz)], inplace=True)
-finder_decisions.drop(finder_decisions.index[~finder_decisions['Receiver_id'].isin(zz)], inplace=True)
+
+receiver_value_counts = finder_decisions['Receiver_id'].value_counts()
+receiver_ids = receiver_value_counts.index.values[receiver_value_counts > 500]
+finder_decisions.drop(finder_decisions.index[~finder_decisions['Receiver_id'].isin(receiver_ids)], inplace=True)
 sender_value_counts = finder_decisions['Sender_id'].value_counts()
-finder_decisions.drop(
-    finder_decisions.index[finder_decisions['Sender_id'].isin(sender_value_counts.index[sender_value_counts == 1])],
-    inplace=True)
-users.drop(users.index[~users.index.isin(np.union1d(finder_decisions['Sender_id'].values, finder_decisions['Receiver_id'].values))], inplace=True)
+sender_ids = sender_value_counts.index.values[sender_value_counts > 500]
+finder_decisions.drop(finder_decisions.index[~finder_decisions['Sender_id'].isin(sender_ids)], inplace=True)
+
+users = users[np.isin(users.index.values,np.union1d(receiver_ids, sender_ids))]
+
+# users.drop(users.index[~users.index.isin(zz)], inplace=True)
+# finder_decisions.drop(finder_decisions.index[~finder_decisions['Sender_id'].isin(zz)], inplace=True)
+# finder_decisions.drop(finder_decisions.index[~finder_decisions['Receiver_id'].isin(zz)], inplace=True)
+# sender_value_counts = finder_decisions['Sender_id'].value_counts()
+# finder_decisions.drop(
+#     finder_decisions.index[finder_decisions['Sender_id'].isin(sender_value_counts.index[sender_value_counts == 1])],
+#     inplace=True)
+# users.drop(users.index[~users.index.isin(np.union1d(finder_decisions['Sender_id'].values, finder_decisions['Receiver_id'].values))], inplace=True)
 users['index'] = np.arange(len(users))
 finder_decisions = finder_decisions.merge(users, how='left', left_on='Sender_id', right_index=True)
 finder_decisions.rename(columns={'age': 'Sender_age', 'gender': 'Sender_gender', 'index': 'Sender_index',
@@ -56,16 +65,17 @@ finder_decisions.rename(columns={'age': 'Receiver_age', 'gender': 'Receiver_gend
                                  'inception': 'Receiver_inception'},
                         inplace=True)
 
-age_encoder = OneHotEncoder()
-age_encoder.fit(users['age'].values.astype(np.int8).reshape(-1, 1))
-age = age_encoder.transform(finder_decisions['Sender_age'].values.astype(np.int8).reshape(-1, 1))
+# age_encoder = OneHotEncoder()
+# age_encoder.fit(users['age'].values.astype(np.int8).reshape(-1, 1))
+# age = age_encoder.transform(finder_decisions['Sender_age'].values.astype(np.int8).reshape(-1, 1))
 
 finder_decisions_train, finder_decisions_valid = train_test_split(finder_decisions, test_size=0.2,
                                                                   stratify=finder_decisions['Sender_index'],
                                                                   random_state=42)
 n_latent_factors=5
 # n_senders = n_receivers = len(zz)
-n_senders = n_receivers = len(users)
+n_senders = len(np.unique(finder_decisions['Sender_id'].values))
+n_receivers = len(np.unique(finder_decisions['Receiver_id'].values))
 sender_input = keras.layers.Input(shape=[1])
 sender_embedding = keras.layers.Embedding(name='sender_embedding', input_dim=n_senders, output_dim=n_latent_factors, input_length=1, embeddings_constraint=non_neg())(sender_input)
 sender_vec = keras.layers.Flatten()(sender_embedding)
@@ -75,27 +85,32 @@ receiver_embedding = keras.layers.Embedding(name='receiver_embedding', input_dim
 receiver_vec = keras.layers.Flatten()(receiver_embedding)
 
 meta_input = keras.layers.Input(shape=[512], name='meta')
-meta_embedding = keras.layers.Embedding(name='meta_embedding', input_dim=n_receivers, output_dim=2, input_length=512, embeddings_constraint=non_neg())(meta_input)
-meta_vec = keras.layers.Flatten()(meta_embedding)
+meta = keras.layers.Dense(128, activation='relu')(meta_input)
+meta = keras.layers.Dense(64, activation='relu')(meta)
+meta = keras.layers.Dense(5, activation='relu')(meta)
+
+# meta_embedding = keras.layers.Embedding(name='meta_embedding', input_dim=n_receivers, output_dim=2, input_length=512, embeddings_constraint=non_neg())(meta_input)
+# meta_vec = keras.layers.Flatten()(meta_embedding)
 
 # meta2_input = keras.layers.Input(shape=[512], name='meta2')
 # meta_embedding = keras.layers.Embedding(name='meta_embedding', input_dim=n_receivers, output_dim=5, input_length=512, embeddings_constraint=non_neg())(meta_input)
 # meta_vec = keras.layers.Flatten()(meta_embedding)
 
-# prod = keras.layers.Dot(axes=0)([sender_vec, receiver_vec])
+prod = keras.layers.Dot(axes=0)([sender_vec, receiver_vec])
+prod = keras.layers.Dense(5, activation='relu')(prod)
+
 # x = keras.layers.Dot(axes=2)([sender_embedding, meta_embedding])
 # prod = keras.layers.Flatten()(prod)
 
 # predicted_preference = keras.layers.Flatten()(prod)
 
-x = keras.layers.Concatenate()([
-    sender_vec,
-                                # receiver_vec,
-    meta_vec,
+x = keras.layers.Concatenate(axis=1)([
+    sender_embedding,
+    receiver_embedding,
+    meta,
     # meta2_input
 ])
 # x = keras.layers.Concatenate()([prod, meta_input])
-x = keras.layers.Dense(2056, activation='relu')(x)
 x = keras.layers.Dense(512, activation='relu')(x)
 x = keras.layers.Dense(256, activation='relu')(x)
 x = keras.layers.Dense(1, activation='sigmoid')(x)
@@ -111,7 +126,7 @@ x = keras.layers.Dense(1, activation='sigmoid')(x)
 # model = keras.Model([sender_input, receiver_input], predicted_preference)
 model = keras.Model([
     sender_input,
-                     # receiver_input,
+                     receiver_input,
                      meta_input,
     # meta2_input
 ], x)
@@ -122,14 +137,14 @@ model.summary()
 
 history = model.fit([
     finder_decisions_train['Sender_index'].values,
-                     # finder_decisions_train['Receiver_index'].values,
+                     finder_decisions_train['Receiver_index'].values,
                      np.stack(finder_decisions_train['Receiver_vgg_face'].values),
 # np.stack(finder_decisions_train['Sender_vgg_face'].values)
 ],
                     finder_decisions_train['Decision'].values,
                     validation_data=([
                                          finder_decisions_valid['Sender_index'].values,
-                                      # finder_decisions_valid['Receiver_index'].values,
+                                      finder_decisions_valid['Receiver_index'].values,
                                       np.stack(finder_decisions_valid['Receiver_vgg_face'].values),
                                          # np.stack(finder_decisions_valid['Sender_vgg_face'].values)
                                      ],
